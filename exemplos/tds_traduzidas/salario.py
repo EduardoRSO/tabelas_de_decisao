704e2ad2ed51fd2c0cb57af650b306cc8e94afbf
erso <edusillva784@gmail.com>
2024-05-16 21:51:04 -0300

    Correção da TD e remoção de acentuação

--- exemplos/tds_traduzidas/salario.py
+++ exemplos/tds_traduzidas/salario.py
@@ -6,15 +6,15 @@ if __name__ == '__main__':
 #TD    Nota1                  0..5
 #TD    Nota2                  6..10
 #TD conditions
-#TD    Salário                Faixa1  Faixa1  Faixa2  Faixa2
-#TD    Avaliação              Nota1   Nota2   Nota1   Nota2
-#TD    Faltas<=10             Y       -       Y       Y
+#TD    Salario                Faixa1  Faixa1    Faixa1  Faixa1  Faixa2  Faixa2  Faixa2  Faixa2
+#TD    Avaliação              Nota1   Nota1     Nota2   Nota2   Nota1   Nota1   Nota2   Nota2     
+#TD    Faltas<=10             Y       N         Y       N       Y       N       Y       N
 #TD actions
-#TD    Salário=P1*Salário     0       0       1       0        
-#TD    Salário=P2*Salário     1       0       0       1
-#TD    Salário=P3*Salário     0       1       0       0
-#TD    Congela(Func)          0       0       2       0          
-#TD    Promove(Func)          2       2       0       2
-#TD    Else_Rule(Func)        0       0       0       0       1
+#TD    Salario=P1*Salario     0       1         0       0       1       1       0       0        
+#TD    Salario=P2*Salario     1       0         0       0       0       0       0       1
+#TD    Salario=P3*Salario     0       0         1       1       0       0       1       0
+#TD    Congela(Func)          0       2         0       0       2       2       0       0          
+#TD    Promove(Func)          2       0         2       2       0       0       2       2
+#TD    Else_Rule(Func)        0       0         0       0       0       0       0       0       1
 #TD end table
     pass
