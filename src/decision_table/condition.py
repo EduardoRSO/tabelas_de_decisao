befc4849f41d25c78b7de342626674d7e2cc07a3
erso <edusillva784@gmail.com>
2024-06-22 16:32:49 -0300

    Parseando condições

--- src/decision_table/condition.py
+++ src/decision_table/condition.py
@@ -1,6 +1,17 @@
+import re
+
 class ConditionParser():
     def __init__(self) -> None:
         pass
 
-    def parse(extracted_decision_table:str) ->list:
-        pass
+    def parse(self, extracted_decision_table:str) ->list:
+        '''
+        Trata os tipos de condições passadas
+        '''
+        conditions = []
+        match  = re.search(r'(#TD conditions.*?#TD actions)',extracted_decision_table, re.DOTALL)
+        if match == None:
+            raise ValueError(f' [-] Erro na busca pela definição do condition: {match} e {extracted_decision_table}')
+        for condition_line in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
+            conditions.append([condition_line[0], condition_line[1:]])
+        return conditions
