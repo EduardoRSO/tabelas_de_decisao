1ba99af31043325130ef78d41436cca8fe0896fb
erso <edusillva784@gmail.com>
2024-06-22 16:33:12 -0300

    Parseando conjuntos

--- src/decision_table/set.py
+++ src/decision_table/set.py
@@ -1,6 +1,21 @@
-class SetParser():
-    def __init__(self) -> None:
-        pass
+import re
 
-    def parse(extracted_decision_table:str) ->list:
-        pass
+class SetParser():
+    def parse(self, extracted_decision_table:str) ->list:
+        '''
+        Trata os tipos de conjuntos passados
+        '''
+        sets = []
+        match  = re.search(r'(#TD sets.*?#TD conditions)',extracted_decision_table, re.DOTALL)
+        if match == None:
+            raise ValueError(f' [-] Erro na busca pela definição do set: {match} e {extracted_decision_table}')
+        for set_name, set_definition in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
+            if '{' in set_definition:
+                sets.append([set_name , f"set([{','.join(set_definition.strip('{}').split(','))}])"])
+            elif '>' in set_definition:
+                sets.append([set_name, set_definition])
+            elif '=' in set_definition:
+                sets.append([set_name, f"=={set_definition.strip('=')}"])
+            elif '..' in set_definition:
+                sets.append([set_name, f"range({set_definition.split('..')[0]},{set_definition.split('..')[1]})"])
+        return sets
