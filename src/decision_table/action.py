bcc4e9dd4d98965a1daf39627c31fa882bff0fab
erso <edusillva784@gmail.com>
2024-06-22 16:32:56 -0300

    Parseando ações

--- src/decision_table/action.py
+++ src/decision_table/action.py
@@ -1,6 +1,17 @@
+import re
+
 class ActionParser():
     def __init__(self) -> None:
         pass 
 
-    def parse(extracted_decision_table:str) ->list:
-        pass
+    def parse(self, extracted_decision_table:str) ->list:
+        '''
+        Trata os tipos de ações passadas
+        '''
+        actions = []
+        match  = re.search(r'(#TD actions.*?#TD end table)',extracted_decision_table, re.DOTALL)
+        if match == None:
+            raise ValueError(f' [-] Erro na busca pela definição da action: {match} e {extracted_decision_table}')
+        for action_line in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
+            actions.append([action_line[0], action_line[1:]])
+        return actions
