d88cc70331e50b52689500377b5f7287077dc5da
erso <edusillva784@gmail.com>
2024-06-25 09:10:11 -0300

    Removi espaços

--- src/decision_table/set.py
+++ src/decision_table/set.py
@@ -11,11 +11,11 @@ class SetParser():
             raise ValueError(f' [-] Erro na busca pela definição do set: {match} e {extracted_decision_table}')
         for set_name, set_definition in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
             if '{' in set_definition:
-                sets.append([set_name , f"set([{','.join(set_definition.strip('{}').split(','))}])"])
+                sets.append([set_name , f"in set([{','.join(set_definition.strip('{}').split(','))}])"])
             elif '>' in set_definition:
                 sets.append([set_name, set_definition])
             elif '=' in set_definition:
-                sets.append([set_name, f"=={set_definition.strip('=')}"])
+                sets.append([set_name, f"== {set_definition.strip('=')}"])
             elif '..' in set_definition:
-                sets.append([set_name, f"range({set_definition.split('..')[0]},{set_definition.split('..')[1]})"])
+                sets.append([set_name, f"in range({set_definition.split('..')[0]},{set_definition.split('..')[1]})"])
         return sets
