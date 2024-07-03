4fee24766b75efa97589b11443b8da80de896ef8
erso <edusillva784@gmail.com>
2024-07-03 08:42:26 -0300

    Uso de regex no parser de conjuntos

--- src/decision_table/set.py
+++ src/decision_table/set.py
@@ -10,12 +10,12 @@ class SetParser():
         if match == None:
             raise ValueError(f' [-] Erro na busca pela definição do set: {match} e {extracted_decision_table}')
         for set_name, set_definition in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
-            if '{' in set_definition:
+            if re.search(r'{.*}',set_definition):
                 sets.append([set_name , f"in set([{','.join(set_definition.strip('{}').split(','))}])"])
-            elif '>' in set_definition:
+            elif re.search(r'>|<|>=|<=',set_definition):
                 sets.append([set_name, set_definition])
-            elif '=' in set_definition:
+            elif re.search(r'=',set_definition):
                 sets.append([set_name, f"== {set_definition.strip('=')}"])
-            elif '..' in set_definition:
+            elif re.search(r'\.\.',set_definition):
                 sets.append([set_name, f"in range({set_definition.split('..')[0]},{set_definition.split('..')[1]})"])
         return sets
