1a9a5a9b39f586e8660bd818efe210e44cbc8aa0
erso <edusillva784@gmail.com>
2024-07-11 22:47:06 -0300

    Adicionei leitura e inserção na base do notion

--- exemplos/tds_traduzidas/sidra.py
+++ exemplos/tds_traduzidas/sidra.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import pandas as pd
 import sidrapy as IBGE
@@ -49,19 +50,66 @@ class handlerIBGE():
 class HandlerDatabase():
 
     def __init__(self):
-        secrets = open('sidra_notion_secrets.text','r').read().split('\n')
-        self._notion_api_secret = secrets[0]
-        self._notion_database_id = secrets[1]
-        self._notion_client = NotionClient(auth=self._notion_api_secret)
+        self._secrets = json.load(open('sidra_notion_secrets.json','r'))
+        self._notion_client = NotionClient(auth=self._secrets['api_secret'])
+        self._database_columns = {
+            'index_register': ['id_index', 'index_name'],
+            'group_register': ['id_group', 'id_parentGroup', 'group_name', 'group_desc'],
+            'composition_register': ['id_group','id_child','factor'],
+            'index_history': ['id_index', 'id_group', 'date', 'item_value', 'item_weight']
+        }
+        self._database_column_type = {
+            'id_index': 'number',
+            'index_name': 'text',
+            'id_group': 'number',
+            'id_parentGroup': 'number',
+            'group_name': 'text',
+            'group_desc': 'text',
+            'id_child': 'number',
+            'factor': 'number',
+            'date' : 'text',
+            'item_value' : 'number',
+            'item_weight' : 'text'
+        }
+        self._database_map_json_path = {
+            'number' : 'number',
+            'text': 'rich_text.0.text.content'
+        }
+
 
     def has_connection(self) ->bool:
-        print(self._notion_client.pages.retrieve(self._notion_database_id))
+        try:
+            for secret_name, database_id in self._secrets.items():
+                if secret_name != 'api_secret':
+                    self._notion_client.databases.retrieve(database_id)
+            return True
+        except Exception as e:
+            print(e)
+            return False
 
-    def get_index_history(index_name:str):
-        pass
-
-    def insert(self):
-        pass
+    def get_database(self, database_name:str):
+        database = self._notion_client.databases.query(self._secrets[database_name])
+        table = []
+        for row in database['results']:
+            column_values = {}
+            for column in self._database_columns[database_name]:
+                column_values[column] = self._safe_get(row, f'properties.{column}.{self._database_map_json_path[self._database_column_type[column]]}') 
+            table.append(column_values)
+        return pd.DataFrame(table) 
+        
+    def insert(self,database_name:str, df:pd.DataFrame):
+        for _, row in df.iterrows():
+            properties = {}
+            for column in self._database_columns[database_name]:
+                properties[column] = self._safe_set(self._database_map_json_path[self._database_column_type[column]],row[column])
+            self._notion_client.pages.create(
+                **{
+                    "parent": {
+                        'database_id': self._secrets[database_name]
+                    },
+                    "properties": properties
+                }
+            )
 
     def is_updated(self):
         pass
@@ -72,7 +120,29 @@ class HandlerDatabase():
     def add_index(self, index_name:str):
         pass
 
-    def retry_connection(self, repetition_counter:int):
+    def _safe_get(self, data:dict, dot_chained_keys:str):
+        keys = dot_chained_keys.split('.')
+        for key in keys:
+            try:
+                if isinstance(data, list):
+                    data = data[int(key)]
+                else:
+                    data = data[key]
+            except (KeyError, TypeError, IndexError):
+                return None
+        return data
+    
+    def _safe_set(self, dot_chained_keys:str, value):
+        keys = dot_chained_keys.split('.')
+        obj = value
+        for key in keys[::-1]:
+            if key.isdigit():
+                obj = [obj]
+            else:
+                obj = {key: obj}
+        return obj
+    
+    def retry_connection(self):
         self.reconnection_tries +=1
 
 class HandlerUpdater():
@@ -113,6 +183,8 @@ class HandlerUpdater():
 
 
 db = HandlerDatabase()
-db.has_connection()
+
+#print(db.get_database('index_register'))
+
 #def analysis_core():
 #    pass
