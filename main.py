74a2085273b1f2b857b4f22fbc5670703dc894f5
erso <edusillva784@gmail.com>
2024-08-12 09:48:25 -0300

    Mudança na main par receber nome do novo arquivo

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import sys
 import os
 from src.workflow_controller import WorkflowController
 
-def start(file_path:str):
-    manager = WorkflowController(file_path)
+def start(file_path:str, file_name:str):
+    manager = WorkflowController(file_path, file_name)
     manager.execute()
 
 def main():
     if len(sys.argv) > 1:
-        file_path = sys.argv[1]
+        file_path, file_name = sys.argv[1], sys.argv[2]
     else:
         file_path = input("Please enter the file path: ")
+        file_name = input("Please enter the file name: ")
 
     if not file_path.endswith('.py'):
         print("Error: The file must have a .py extension.")
@@ -20,7 +21,7 @@ def main():
         print("Error: The file does not exist.")
         return
 
-    start(file_path)
+    start(file_path, file_name)
 
 if __name__ == "__main__":
     main()
