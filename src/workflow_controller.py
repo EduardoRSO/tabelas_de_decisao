39c6272e62fed083d4c5569dd17c578608478a1e
erso <edusillva784@gmail.com>
2024-08-12 09:48:04 -0300

    Mudanças no workflow pela redistribuição de responsabilidades

--- src/workflow_controller.py
+++ src/workflow_controller.py
@@ -4,8 +4,9 @@ from src.code_generator.code_generator import CodeGenerator
 from src.code_inserter.code_inserter import CodeInserter
 
 class WorkflowController:
-    def __init__(self, file_path):
+    def __init__(self, file_path:str, file_name:str):
         self.file_path = file_path
+        self.file_name = file_name
         self.code_reader = None
         self.decision_tables_found = []
         self.processed_tables = []
@@ -16,26 +17,25 @@ class WorkflowController:
         # Initialize and use CodeReader to read the code
         self.code_reader = CodeReader(self.file_path)
         self.decision_tables_found = self.code_reader.get_extracted_decision_tables()
+        self.end_position_of_decision_tables_found = self.code_reader.get_extracted_decision_tables_positions()
 
     def process_decision_tables(self):
         # Initialize and use DecisionTable to process each decision table
         self.processed_tables = []
-        for table in self.decision_tables_found:
-            decision_table = DecisionTable(table)
-            self.processed_tables.append(decision_table.process())
+        for index, table in enumerate(self.decision_tables_found):
+            self.processed_tables.append(DecisionTable(table, self.end_position_of_decision_tables_found[index]))
 
     def generate_code(self):
         # Initialize and use CodeGenerator to generate code for each processed table
         self.code_generator = CodeGenerator()
         self.generated_code = []
         for table in self.processed_tables:
-            generated_code = self.code_generator.generate(table)
-            self.generated_code.append(generated_code)
+            self.generated_code.append(self.code_generator.generate_code(table))
 
     def insert_code(self):
         # Initialize and use CodeInserter to insert the generated code into the file
-        code_inserter = CodeInserter(self.file_path)
-        code_inserter.insert(self.generated_code)
+        code_inserter = CodeInserter(self.file_path, self.file_name)
+        code_inserter.insert( {k:v for k,v in zip([table.end_position_of_decision_table for table in self.processed_tables], self.generated_code)})
 
     def execute(self):
         # Execute the entire workflow
@@ -43,9 +43,3 @@ class WorkflowController:
         self.process_decision_tables()
         self.generate_code()
         self.insert_code()
-
-# Example usage
-if __name__ == "__main__":
-    file_path = "path/to/your/file.py"
-    manager = WorkflowController(file_path)
-    manager.execute()
