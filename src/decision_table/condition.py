999457e0ad807d3274666560cff82fa6100d6c3f
erso <edusillva784@gmail.com>
2024-08-25 16:33:43 -0300

    Auto-documentação condition.py

--- src/decision_table/condition.py
+++ src/decision_table/condition.py
@@ -1,12 +1,12 @@
 #1[
-#1 TITULO: condition.py
+#1 TITULO: CONDITION.PY
 #1 AUTOR: EDUARDO RIBEIRO SILVA DE OLIVEIRA
-#1 DATA: 26/07/2024
+#1 DATA: 25/08/2024
 #1 VERSAO: 1
-#1 FINALIDADE: Processar e extrair condições de uma tabela de decisão formatada como string.
-#1 ENTRADAS: extracted_decision_table (str) - String contendo a tabela de decisão extraída.
-#1 SAIDAS: conditions (list) - Lista de condições extraídas da tabela de decisão.
-#1 ROTINAS CHAMADAS: __init__, parse
+#1 FINALIDADE: PROCESSAR E EXTRAIR CONDICOES DE UMA TABELA DE DECISAO FORMATADA COMO STRING
+#1 ENTRADAS: EXTRACTED_DECISION_TABLE (STR) - STRING CONTENDO A TABELA DE DECISAO EXTRAIDA
+#1 SAIDAS: CONDITIONS (LIST) - LISTA DE CONDICOES EXTRAIDAS DA TABELA DE DECISAO
+#1 ROTINAS CHAMADAS: __INIT__, PARSE
 #1]
 
 import re
@@ -15,7 +15,7 @@ class ConditionParser():
     
     #1[
     #1 ROTINA: __init__
-    #1 FINALIDADE: Inicializar a instância da classe ConditionParser.
+    #1 FINALIDADE: INICIALIZAR A INSTANCIA DA CLASSE CONDITIONPARSER
     #1 ENTRADAS: N/A
     #1 DEPENDENCIAS: N/A
     #1 CHAMADO POR: N/A
@@ -24,31 +24,31 @@ class ConditionParser():
     #2[
     #2 PSEUDOCODIGO DE: __init__
     def __init__(self) -> None:
-    #2  Inicializa a instância sem configurações adicionais
+    #2  INICIALIZA A INSTANCIA SEM CONFIGURACOES ADICIONAIS
         pass
     #2]
 
     #1[
-    #1 ROTINA: parse
-    #1 FINALIDADE: Extrair e processar as condições da tabela de decisão fornecida como string.
-    #1 ENTRADAS: extracted_decision_table (str) - String contendo a tabela de decisão extraída.
-    #1 DEPENDENCIAS: re (módulo de expressões regulares)
+    #1 ROTINA: PARSE
+    #1 FINALIDADE: EXTRAIR E PROCESSAR AS CONDICOES DA TABELA DE DECISAO FORNECIDA COMO STRING
+    #1 ENTRADAS: EXTRACTED_DECISION_TABLE (STR) - STRING CONTENDO A TABELA DE DECISAO EXTRAIDA
+    #1 DEPENDENCIAS: RE (MODULO DE EXPRESOES REGULARES)
     #1 CHAMADO POR: N/A
     #1 CHAMA: N/A
     #1]
     #2[
     #2 PSEUDOCODIGO DE: parse
-    def parse(self, extracted_decision_table:str) ->list:
-    #2  Trata os tipos de condições passadas
+    def parse(self, extracted_decision_table: str) -> list:
+    #2  TRATA OS TIPOS DE CONDICOES PASSADAS
         conditions = []
-    #2  Procura a seção da tabela de decisão que contém as condições e ações
-        match  = re.search(r'(#TD conditions.*?#TD actions)', extracted_decision_table, re.DOTALL)
-    #2  Levanta uma exceção se a definição de condições não for encontrada
-        if match == None:
-            raise ValueError(f' [-] Erro na busca pela definição do condition: {match} e {extracted_decision_table}')
-    #2  Itera sobre as linhas de condições extraídas e as adiciona à lista
+    #2  PROCURA A SECAO DA TABELA DE DECISAO QUE CONTEM AS CONDICOES E ACOES
+        match = re.search(r'(#TD conditions.*?#TD actions)', extracted_decision_table, re.DOTALL)
+    #2  LEVANTA UMA EXCECAO SE A DEFINICAO DE CONDICOES NAO FOR ENCONTRADA
+        if match is None:
+            raise ValueError(f' [-] Erro na busca pela definicao do condition: {match} e {extracted_decision_table}')
+    #2  ITERA SOBRE AS LINHAS DE CONDICOES EXTRAIDAS E AS ADICIONA A LISTA
         for condition_line in [line.split()[1:] for line in match.group(0).split('\n')][1:-1]:
             conditions.append([condition_line[0], condition_line[1:]])
-    #2  Retorna a lista de condições
+    #2  RETORNA A LISTA DE CONDICOES
         return conditions
     #2]
